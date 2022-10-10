72ebb18ef89563ba493620e258f636fc07199773
root <root@p-7806a847-8224-4a37-aed9-a01016da79eb.projects-domain.projects.svc.cluster.local>
2022-10-10 09:05:45 +0000

    manual deepnote streamlit update commit

--- streamlit.py
+++ streamlit.py
@@ -28,7 +28,7 @@ def first_try():
     st.markdown("## Simple Data Analysis Demo:")
 
     st.markdown("Below is an interactive example of how our football passing network models work:")
-    competition = st.radio("Choose the competition here (Currently only tested FIFA World Cup)", ('FIFA World Cup'))
+    competition = st.radio("Choose the competition here (Currently only tested FIFA World Cup)", ('FIFA World Cup', "Don't Touch this One"))
 
     if competition == "FIFA World Cup":
         comp = "FIFA World Cup"
